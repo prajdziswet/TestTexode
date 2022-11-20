1db7ec1499c82744e912900ffd0c0a58c461855c
Уладзімір Косаў <prajdziswet@mail.ru>
2022-11-20 06:07:42 +0300

    прівязка полей

---
+++ WpfTest/WpfTest/KeyObj.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTest
+{
+    public class KeyObj : INotifyPropertyChanged
+    {
+        public KeyObj(int selectCurrency, DateTime date1, DateTime date2)
+        {
+            _dateStart = date1;
+            _dateFihish = date2;
+        }
+
+        private readonly ObservableCollection<KeyValuePair<string, string>> _CurrencyName = new ObservableCollection<KeyValuePair<string, string>> 
+        { new KeyValuePair<string, string>("USD","USD"),
+            new KeyValuePair<string, string>("EUR","EUR"),
+            new KeyValuePair<string, string>("RUB","РосRUB"),
+            new KeyValuePair<string, string>("BTC","BTC")
+            };
+
+    //private int _selesctItemInCurrency = 1;
+        //public string Get_currencyName { get => _currencyName[_selesctItemInCurrency - 1]; }
+        public ObservableCollection<KeyValuePair<string, string>> CurrencyName
+        {
+            get => _CurrencyName;
+        }
+
+        private string _selectCurrency="USD";
+
+        public string SelectCurrency
+        {
+            get => _selectCurrency;
+            set
+            {
+                _selectCurrency=value;
+            }
+        }
+
+        private DateTime _dateStart;
+
+        public DateTime dateStart
+        {
+            get => _dateStart;
+            set
+            {
+                _dateStart= value;
+                OnPropertyChanged("dateStart");
+            }
+        }
+
+        private DateTime _dateFihish;
+
+        public DateTime dateFinish
+        {
+            get => _dateFihish;
+            set
+            {
+                _dateFihish = value;
+                OnPropertyChanged("dateStart");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
--- WpfTest/WpfTest/MainWindow.xaml.cs
+++ WpfTest/WpfTest/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace WpfTest
 {
@@ -20,9 +21,44 @@ namespace WpfTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        private KeyObj keyObj;
         public MainWindow()
         {
             InitializeComponent();
+            keyObj = new KeyObj(1,DateTime.Now, DateTime.Now);
+            DataContext = keyObj;
+            //comboBox1.ItemsSource = keyObj.CurrencyName;
+        }
+
+        private void WindowsFormsHost_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawGrafic();
+        }
+
+        private void DrawGrafic()
+        {
+            // Все графики находятся в пределах области построения ChartArea, создадим ее
+            chart.ChartAreas.Add(new ChartArea("Default"));
+            chart.ChartAreas[0].AxisX.Title = "Дата";
+
+            chart.ChartAreas[0].AxisY.Title = "Курс";
+
+
+            // Добавим линию, и назначим ее в ранее созданную область "Default"
+            chart.Series.Add(new Series("Series1"));
+            chart.Series["Series1"].ChartArea = "Default";
+            chart.Series["Series1"].ChartType = SeriesChartType.Line;
+
+
+            // добавим данные линии
+            string[] axisXData = new string[] { "a", "b", "c" };
+            double[] axisYData = new double[] { 0.1, 1.5, 1.9 };
+            chart.Series["Series1"].Points.DataBindXY(axisXData, axisYData);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var z=keyObj.SelectCurrency;
         }
     }
 }
